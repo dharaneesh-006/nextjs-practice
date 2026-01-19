ad18f4657ca44718f9fa8dbf3c1fbf13abba5428
dharaneesh-006 <dharaneesh112@gmail.com>
2026-01-19 10:51:20 +0530

    Routing in NextJS

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-export default function Home() {
-  console.log("Hello")
-  return (
-    <>
-      <h1>Hello NExtJS</h1>
-    </>
-  );
+import Link from "next/link";
+
+export default function Home()
+{
+  return(
+    <main className="p-2.5">
+      <h1>Home Page</h1>
+      <nav className="mt-2">
+        <ul>
+          <li>
+            <Link href="/about" > Go to About</Link>
+          </li>
+          <li>
+            <Link href="/contact" > Go to contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+    </main>
+  )
 }
